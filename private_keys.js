7a6a9179eb1e3e2fdc99ace51d30a9ad84547c7b
NickSpinosa <nickspinosa1022@gmail.com>
2016-11-12 18:05:49 -0500

    first pass for auth0 back end

---
+++ private_keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AUTH0_DOMAIN: 'rapport.auth0.com',
+    AUTH0_CLIENT_ID: 'pA75v0B8UDfNOk0h2tDnz5in4Je3AZHL',
+    AUTH0_CLIENT_SECRET: 'JhZqRFI-GB77-WMZMxX2ZzzwlVFl75olMk0ihmK7YpMCM0i9P07_WA9SgtX9611h'
+};
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,6 @@
 // REQUIRE CONTROLLERS
 // var exampleController = require('../example/exampleController.js');
+const auth0Utils = require('../utils/auth0_utils.js');
 module.exports = function (app, express) {
 
 
@@ -8,6 +9,11 @@ app.get('/',function(request, response){
   response.status(200).send('Server Alive');
 });
 
+//route for handling sign in and sign up
+app.get('/signing', (req, res) => {
+  auth0Utils.getUserIdFromToken(req.body.id)
+    .then(userId => console.log('successfully got user id', userId ));
+});
 // app.get
 //app.get('/api/example', exampleController.exampleMethod);
 
