e944314206daa0a90ab5f08ba16d36c9d3b26efa
Justin <jproche5@gmail.com>
2016-11-13 18:23:56 -0500

    moved login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Auth } from '../shared/auth.service';
+
 @Component({
   selector: 'my-app',
   styleUrls: ['app/app/app.component.css'],
+  providers: [Auth],
   template: `
     <h1>{{title}}</h1>
     <nav>
+        <button class="btn btn-primary btn-margin" (click)="auth.login()" *ngIf="!auth.authenticated()">Log In</button>
+        <button class="btn btn-primary btn-margin" (click)="auth.logout()" *ngIf="auth.authenticated()">Log Out</button>
         <p>This is a change//</p>
         <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
         <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
@@ -14,5 +19,7 @@ import { Component } from '@angular/core';
   
 })
 export class AppComponent {
+  constructor(private auth: Auth) {}
+
   title = 'Tour of Heroes';
 }
