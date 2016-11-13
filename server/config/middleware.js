aa4988c2f5cb2f74ef654622d0fe0193b8cbc74a
NickSpinosa <nickspinosa1022@gmail.com>
2016-11-12 20:50:30 -0500

    auth0 backend

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,5 +4,8 @@ module.exports = function(app, express){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
+  app.use((req,res,next) => {
+    console.log(`req.method AT req.url`);
+  });
   app.set('port', (process.env.PORT || 5050));
 }
