7633ccd182389f6f7db59cda5f85000a6ca573ab
Nam Huynh <nthwinn@gmail.com>
2016-11-16 13:23:39 -0700

    updated botctrl to handle new obj format from db and checkt for platform type

--- server/bot/botController.js
+++ server/bot/botController.js
@@ -30,6 +30,37 @@ module.exports = {
       }
       runGenerator(tasks.length, 0);
     });
+  },
+  runAllTasks: function(){
+    dbModel.tasks.getAllTasks(function(tasks){
+       if(err){ throw err;}
+      console.log('tasks', tasks);
+      console.log('email', tasks[0].recipient.name);
+
+      function runGenerator(length, index) {
+        if(length === index) {
+          return;
+        }
+        console.log(index)
+        // Run Gmail Tasks
+        if(tasks[index].tasks.platform === gmail){
+          var msgData = {
+            username: tasks[index].user.userName,
+            useremail: tasks[index].user.userEmail,
+            emailTo: tasks[index].recipient.email,
+            subject: "whooooot! this is from Nam",
+            body: 'Hello there! How have you been?<br/>'
+          }
+          gmail.sendMailBot(msgData, function(results){
+            console.log('results',results);
+            runGenerator(length, index+1);
+          });
+        } else if(tasks[index].tasks.platform === facebook){
+          //Run FaceBook Tasks
+        }
+      }
+      runGenerator(tasks.length, 0);
+    });
   }
 }
 
