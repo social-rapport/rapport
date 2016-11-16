ec3168ccda7e19ebf4b01c75b726cf77c93e2c7a
Justin <jproche5@gmail.com>
2016-11-15 20:44:42 -0500

    renamed private_keys to env

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: process.env.DBPASS || require('../../private_keys.js').DB_PASS,
+  password: process.env.DBPASS || require('../../env.js').DB_PASS,
   database: 'rapport'
 });
 
