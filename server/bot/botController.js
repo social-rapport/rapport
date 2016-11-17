b1910ec392452ed418345bf002042173a60351ea
Nam Huynh <nthwinn@gmail.com>
2016-11-16 20:52:00 -0700

    botctrl sendGmail working

--- server/bot/botController.js
+++ server/bot/botController.js
@@ -1,5 +1,6 @@
 var dbModel = require('../db/dbModel.js');
 var gmail = require('../config/gmailController.js');
+
 var express = require('express');
 var app = express();
 
@@ -23,28 +24,31 @@ module.exports = {
           subject: "whooooot! this is from Nam",
           body: 'Hello there! How have you been?<br/>'
         }
-        gmail.sendMailBot(msgData, function(results){
-          console.log('results',results);
-          runGenerator(length, index+1);
-        });
+        // gmail.sendMailBot(msgData, function(results){
+        //   console.log('results',results);
+        //   runGenerator(length, index+1);
+        // });
       }
       runGenerator(tasks.length, 0);
     });
   },
-  runAllTasks: function(){
-    dbModel.tasks.getAllTasks(function(tasks){
-       if(err){ throw err;}
+  runAllTasks: function(callback){
+    console.log('runAllTasks running');
+    dbModel.tasks.getTasksForChronJob(function(tasks){
       console.log('tasks', tasks);
       console.log('email', tasks[0].recipient.name);
       var completedTasks = [];
 
       function runGenerator(length, index) {
         if(length === index) {
+          module.exports.logTasks(completedTasks);
+          callback('Hey I\'m done');
           return;
         }
         console.log(index);
         // Run Gmail Tasks
-        if(tasks[index].tasks.platform === gmail){
+        if(tasks[index].tasks.platform === 'gmail'){
+          console.log('platform', tasks[index].tasks.platform);
           var msgData = {
             username: tasks[index].user.userName,
             useremail: tasks[index].user.userEmail,
@@ -52,19 +56,20 @@ module.exports = {
             subject: "whooooot! this is from Nam",
             body: 'Hello there! How have you been?<br/>'
           }
-          gmail.sendMailBot(msgData, function(results){
+          gmail.sendMailBot(msgData, tasks[index].user.oauth, function(results){
             console.log('results',results);
             completedTasks.push(tasks[index].tasks);
             runGenerator(length, index+1);
 
           });
-        } else if(tasks[index].tasks.platform === facebook){
+        } else if(tasks[index].tasks.platform === 'facebook'){
           //Run FaceBook Tasks
           completedTasks.push(tasks[index].tasks);
         }
       }
       runGenerator(tasks.length, 0);
-      logTasks(completedTasks);
+
+
     });
   },
   logTasks: function(tasks){
@@ -83,7 +88,10 @@ module.exports = {
   }
 }
 
-// unit test
+// // unit test runAllTasks
+// module.exports.runAllTasks();
+
+// unit test logTasks
 // var tasks = [{
 //     date: '2016-11-15',
 //     platform: 'gmail',
@@ -100,7 +108,6 @@ module.exports = {
 //     id_bot: 2,
 //     id_recipient: 2,
 // }];
-
 
 // module.exports.logTasks(tasks, function(result){
 //   console.log(result);
