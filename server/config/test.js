5fa8c8d4abdd5abbcc2932c30f08d4a12b29e763
Justin <jproche5@gmail.com>
2016-11-18 11:17:33 -0500

    refactors lengthToBool in dbModel

--- server/config/test.js
+++ server/config/test.js
@@ -1,34 +1,39 @@
 const auth0Utils = require('../utils/auth0_utils.js');
 var token = require('../../env.js').ADMIN_IDTOKEN;
 var dbModel = require('../db/dbModel.js');
+var db = require('../db/db.js');
 
-auth0Utils.userObjFromToken(token,function(userObj){
+
+//dbModel.database.drop(function(data){
+    dbModel.database.init();
+//})
+// auth0Utils.userObjFromToken(token,function(userObj){
     
-})
+// })
 
-function resolveUserAction((userObj){
-    dbModel.gmail.emailExists(userObj.email, function(exists){
-        if(exists){
-            getUserData(userObj);
-        } else if (!exists){
-            //saveNewUser(userObj);
-        }
-    });
-})
+// function resolveUserAction((userObj){
+//     dbModel.gmail.emailExists(userObj.email, function(exists){
+//         if(exists){
+//             getUserData(userObj);
+//         } else if (!exists){
+//             //saveNewUser(userObj);
+//         }
+//     });
+// })
 
-function saveNewUser(userObj){
-    dbModel.users.saveNewUser(userObj, function(saved){
-        return {
-            username: userObj.name,
-            email: userObj.email,
-            newUser: true,
-            };
-        });
-}
+// function saveNewUser(userObj){
+//     dbModel.users.saveNewUser(userObj, function(saved){
+//         return {
+//             username: userObj.name,
+//             email: userObj.email,
+//             newUser: true,
+//             };
+//         });
+// }
 
-function getUserData(){
-    dbModel.users.getBasicUserData(userObj.email, function(info){
-        return info;
-    });
-}
+// function getUserData(){
+//     dbModel.users.getBasicUserData(userObj.email, function(info){
+//         return info;
+//     });
+// }
 
