93439804f4469a3663a2d86cb7e21bdd7d8dce2b
Justin <jproche5@gmail.com>
2016-11-19 17:53:14 -0500

    fixed conflict

--- server/config/test.js
+++ server/config/test.js
@@ -4,36 +4,9 @@ var dbModel = require('../db/dbModel.js');
 var db = require('../db/db.js');
 //var _dbModel = require('../db/_dbModel.js');
 
-var contacts = [
-    {
-     email: 'a@b',
-     name: 'John',
-     photo: 'xyz',
-    }
-];
 
-var bot1 = {
-            "bots":[{
-            "botType":'basic',
-            "tasks":[
-                'sayHappyBirthdayGmail',
-                'sayHappyBirthdayFacebook',
-                'sayHiGmail',
-                'sayHiFacebook'
-            ],
-            "selectedContacts":contacts,
-            botActivity:{
-                "recent":[],
-                "scheduled":[]
-            }
-            }]
-        };
 
 dbModel.tasks.addBotToUser(bot1.bots[0],1,function(res){
   console.log(res);
 });
-// dbModel.tasks.updateTasksFlow(bot1,1,function(res){
-//             console.log('response is',res);
-
-//         });
 
--- spec/dbBotSpec.js
+++ spec/dbBotSpec.js
@@ -27,7 +27,7 @@ var getUsers = function(){ return connection.query('select * from users')};
 var getBots = function(){ return connection.query('select * from bot');}
 var getTasks = function(){ return connection.query('select * from bot');};
 var deleteBots = function(){ return connection.query('delete from bot')};
-var deleteTasks = function(){ return connection.query('delete from Tasks')};
+var deleteTasks = function(){ return connection.query('delete from Tasks where ')};
 var deleteUsers = function(){ return connection.query('delete from users')};
 var deleteGmail = function(){ return connection.query('delete from gmail')};
 
@@ -52,149 +52,137 @@ var contacts = [
 ];
 
 var bot1 = {
-            "bots":[{
-            "botType":'basic',
-            "tasks":[
-                'sayHappyBirthdayGmail',
-                'sayHappyBirthdayFacebook',
-                'sayHiGmail',
-                'sayHiFacebook'
-            ],
-            "selectedContacts":contacts,
-            botActivity:{
-                "recent":[],
-                "scheduled":[]
-            }
-            }]
-        };
+    "bots":[{
+    "botType":'basic',
+    "tasks":[
+        'sayHappyBirthdayGmail',
+        'sayHappyBirthdayFacebook',
+        'sayHiGmail',
+        'sayHiFacebook'
+    ],
+    "selectedContacts":contacts,
+    botActivity:{
+        "recent":[],
+        "scheduled":[]
+    }
+    }]
+};
 
 var contacts2 = [
-    {
-        email: 'b@c',
-        name: 'Jane',
-        photo: '123',
-    }
-]
+        {
+            email: 'b@c',
+            name: 'Jane',
+            photo: '123',
+        }
+    ]
 
-var bot2 = {"bots":[
-    {"botType":"basic",
-    "tasks":[
-        "sayHappyBirthdayGmail",
-        "sayHappyBirthdayFacebook",
-        "sayHiGmail","sayHiFacebook"],
-    "selectedContacts":[],
-    "botActivity":{
-        "recent":[],
-        "scheduled":[]}
-}]};
+var bot2 = {
+    "bots":[
+        {"botType":"basic",
+        "tasks":[
+            "sayHappyBirthdayGmail",
+            "sayHappyBirthdayFacebook",
+            "sayHiGmail","sayHiFacebook"],
+        "selectedContacts":[],
+        "botActivity":{
+            "recent":[],
+            "scheduled":[]}
+        }]
+    };
         
 //<-------------------BEFORE-EACH------------------->
-
-//this errors for some reason, but still deletes
-// beforeEach(function(done){
-//     deleteTasks()
-//     .then(deleteBots)
-//     .then(done)
-//     .catch(done);
-// });
+beforeEach(function(done){
+    _dbModel.deleteBot(1,'basic',function(res){
+        console.log(res);
+        done();
+    })
+});
 
 //<-------------------USER DATA------------------->
 //todo: server responds with 200
 
-xdescribe ('identifying user',function(){
+describe ('identifying user',function(){
 
-  it('converts tokens to emails and auth0ids',function(done){
+  xit('converts tokens to emails and auth0ids',function(done){
     auth0.getUserIdFromToken(idToken)
     .then(function(a,b){
+        console.log(a);
         expect(a.email);
     })
     .then(done)
     .catch(done)
   });
 
-  it('gets a userObject from a token', function(done){
+  xit('gets a userObject from a token', function(done){
     auth0.userObjFromToken(idToken,function(res){
         expect(res.email);
         done();
     });
   });
 
-  it('gets a database id from email',function(done){
+  xit('gets a database id from email',function(done){
       dbModel.users.getIdFromEmail(emailInfo.email,function(id){
           expect(id.length);
           done();
       })
   })
-});
 
-describe ('getting data about the user', function(){
-    xit('gets basic user data',function(done){
+  it('gets basic user data',function(done){
         dbModel.users.getBasicUserData(emailInfo.email,function(data, err){
             console.log(data, err);
             expect(data.email).to.equal(emailInfo.email);
             done();
         })
     });
-})
-
+});
 
 //<-------------------GETTING BOTS------------------->
 
 describe('getting bots for the user',function(){
-    it('gets all the bot information for a user',function(done){
-
+    xit('gets all the bot information for a user',function(done){
+        //not yet implemented
     })
 });
 
 //<-------------------ADDING AND REMOVING BOTS------------------->
 describe('knows whether bots exist for a user and botType',function(){
-    it('returns 0 when there are no bots for a user',function(done){
-            dbModel.bot.exists(1, function(res){
-                console.log(res);
-                expect(res).to.equal(false);
-                done();
-            });
+    xit('returns 0 when there are no bots for a user',function(done){
+        dbModel.bot.exists(1, function(res){
+            console.log(res);
+            expect(res).to.equal(false);
+            done();
+        });
     });
 });
 
-
 describe('adding and removing bots',function(){
-    it('adds a single bot to an empty user with a user Id',function(done){
+    xit('adds a single bot to an empty user with a user Id',function(done){
         dbModel.tasks.updateTasksFlow(bot1,1,function(res){
-            expect(res).to.equal('works');
+            _dbModel.getBots(emailInfo.email,function(res){
+               console.log(res);
+               expect(res.length).to.equal(1);
+           })
         });
     });
 
-    it('removes a single bot by name and userId',function(done){
-            _dbModel.deleteBot(1,'basic',function(res){
-                console.log(res);
-            })
-
-        
+    xit('removes a single bot by name and userId',function(done){
+        _dbModel.deleteBot(1,'basic',function(res){
+           _dbModel.getBots(emailInfo.email,function(res){
+               console.log(res);
+               expect(res.length).to.equal(0);
+           })
+        })
     })
     
 });
 
-xdescribe('deleting bots for the user',function(){
-
-    xit('can delete a bot that has existing tasks',function(done){
-
-    })
-});
-
-
 //<-------------------GETTING TASKS------------------->
-
 describe ('getting tasks for a single botName and userId',function(){
-
     xit('gets tasks for a bot',function(done){
-        
-        dbModel.bots._getTasks('basic',1, function(results){
+        dbModel.bots._getTasks(1, 'basic',function(results){
+
         });
-
     });
-
-
 });
 
 
