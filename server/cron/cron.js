4b7a0cc35bf7684c7f9d76bdb983a770cfeb01f1
Vi Vo <vi.uyen.vo@gmail.com>
2016-11-28 16:32:04 -0800

    run cron everyday at 9 am

--- server/cron/cron.js
+++ server/cron/cron.js
@@ -2,7 +2,7 @@ var bot = require('../bot/botController.js');
 var CronJob = require('cron').CronJob;
 
 module.exports.runCron = function(){
-  new CronJob('* * * * * *', function() {
+  new CronJob('0 0 9 * * *', function() {
     console.log('Running Tasks');
     bot.runAllTasks(bot.logTasks);
   }, null, true, 'America/Los_Angeles');
