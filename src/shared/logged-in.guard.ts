7501fcd3cc0c89fab4d46cb9bb30182811553c5e
NickSpinosa <nickspinosa1022@gmail.com>
2016-11-30 19:37:30 -0500

    implements log in guard

---
+++ src/shared/logged-in.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { Auth } from '../shared/auth.service';
+import { Component } from '@angular/core';
+
+@Injectable()
+
+export class LogInGuard implements CanActivate {
+  constructor(private auth: Auth, private router: Router){
+
+  }
+
+  canActivate() {
+    if(localStorage.getItem('id_token')) {
+      this.router.navigate(['loading']);
+      this.auth.onAuthentication({idToken: localStorage.getItem('id_token')});
+      return false;
+    } else {
+      return true;
+    }
+    
+  //return true;
+  }
+}
